bd492cb992e2eb43641105b99ba64d7be24cf5ca
Antonio Escalante <aaescalante@uh.edu>
2020-11-25 01:49:23 +0000

    - cleaned up minimap -created key, keyholder, and keydoor script -added spawners to middel room in hades top left room and courtyard -added doors to each room -

---
+++ Assets/Scripts/Game Scripts/Key.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] private KeyType keyType;
+  public enum KeyType
+    {
+        room1,
+        room2,
+        room3,
+        room4,
+        Hades
+    }
+
+    public KeyType GetKeyType()
+    {
+        return keyType;
+    }
+}
