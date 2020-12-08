174782193615f817d01b91636ee11a5e8e09ebd6
Antonio Escalante <aaescalante@uh.edu>
2020-12-08 04:43:39 +0000

    -added lava trigger to do .25 damage to player and tibnt player red.

---
+++ Assets/Scripts/Game Scripts/lava.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lava : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        GameObject player = GameObject.Find("Promethesus");
+
+        if (collider.gameObject.tag == "Player")
+        { 
+                GameManager.Instance.updateHP(-.25f);
+                CinemachineShake.Instance.ShakeCamera(5f, .3f);
+                player.GetComponent<SpriteRenderer>().color = new Color(255, 0, 0);            
+        }
+        else
+        {
+            player.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255);
+        }
+    }
+
+}
