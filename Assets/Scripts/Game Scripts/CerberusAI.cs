5aaf5ecee97e84de0a0d7e767e9124be643e177a
anticyclogenesis <chasebowman13@gmail.com>
2020-12-10 15:37:03 +0000

    -Lava logic implemented

--- Assets/Scripts/Game Scripts/CerberusAI.cs
+++ Assets/Scripts/Game Scripts/CerberusAI.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class CerberusAI : MonoBehaviour
 {
@@ -10,7 +12,7 @@ public class CerberusAI : MonoBehaviour
     [SerializeField] private float health = 100f;
     [SerializeField] private float stunnedTime = 2f;
     [SerializeField] private float maxHealth = 100f;
-    [SerializeField] private float lavaFallDelay = 1.5f;
+    [SerializeField] private float lavaFallDelay = 0.01f;
 
 
     private ParticleSystem blood;
@@ -28,14 +30,14 @@ public class CerberusAI : MonoBehaviour
     public int phase = 1;
     private bool lavaFlow = false;
     public GameObject lavaPreFab;
-    private GameObject[] lavaTiles;
-    private List<List<GameObject>> listColumns = new List<List<GameObject>>(30);
-    private List<GameObject> firstlist = new List<GameObject>();
-    private int numTiles = 0;
+    //private List<GameObject> activeLavaPrefabs = new List<GameObject>();
+    private int tileNum = 1;
     private int colNum = 0;
     private float colXStart = 98.5016f;
     private float colXMultiplier = 0.9899f;
-    colPara[] paraList;
+    private StructData[] struct31List = new StructData[31];
+    private bool startLava;
+    private GameObject[] tilesToDelete;
 
     // Start is called before the first frame update
     void Start()
@@ -47,7 +49,7 @@ public class CerberusAI : MonoBehaviour
         blood = gameObject.GetComponentInChildren(typeof(ParticleSystem), true) as ParticleSystem;
         localScale = healthBar.transform.localScale;
         SFX = GameObject.Find("SFX");
-        paraList = prepareLava();
+        prepareLava();
     }
 
     // Update is called once per frame
@@ -86,15 +88,19 @@ public class CerberusAI : MonoBehaviour
 
     private void Phase1()
     {
-        animation.SetBool("IsStun", true);
+        animation.SetBool("IsIdle", true);
+        StartCoroutine("Phase1SubDelay");
+    }
+    IEnumerator Phase1SubDelay()
+    {
+        yield return new WaitForSeconds(10f);
+
         if (!lavaFlow)
         {
-            //releaseLava();
             lavaFlow = true;
             StartCoroutine("releaseLava");
         }
     }
-
     private void Phase2()
     {
         animation.SetBool("IsHowl", true);
@@ -104,115 +110,246 @@ public class CerberusAI : MonoBehaviour
     {
         animation.SetTrigger("Swipe");
     }
-
-    private colPara[] prepareLava()
+    struct StructData
     {
-        colPara[] colParaList = new colPara[1];
-        for(int colParaIt = 0; colParaIt < colParaList.Length; ++colParaIt)
-        {
-            colPara colParaCurrent;
-            colParaCurrent.numTiles = numTiles;
-            colParaCurrent.skip = 1;
-            colParaCurrent.xPos = colParaIt;
-            colParaList[colParaIt] = colParaCurrent;
-            //Debug.Log("numTiles: " + numTiles);
-            //Debug.Log("xPos:" + colParaIt);
-        }
-
-        return colParaList;
-    }
-    //IEnumerator releaseLava()
-    //{
-    //    yield return new WaitForSeconds(lavaFallDelay);
-
-    //    for (int col = 0; col < paraList.Length; ++col)
-    //    {
-    //        //StartCoroutine("lavaCol25", paraList[col]);
-    //        lavaCol25(paraList[col]);
-    //    }
-    //    //array3 = new int[] { 1, 3, 5, 7, 9 };
-    //    ++numTiles;
-    //}
-    IEnumerator releaseLava()
-    {
-        yield return new WaitForSeconds(lavaFallDelay);
-        for (int col = 0; col < paraList.Length; ++col)
-        {
-            firstlist.Add(Instantiate(lavaPreFab, new Vector3(colXStart + (colXMultiplier * paraList[col].xPos), -15.644f, 14), Quaternion.identity) as GameObject);
-            GameObject tile = firstlist[col];
-            Debug.Log("Tile pos: " + tile.transform.position);
-
-
-            if (tile.transform.position.y + 29.71f < 0.01)
-            {
-                Destroy(tile);
-                firstlist.Remove(tile);
-                Debug.Log("Destroy!");
-            }
-            else
-            {
-                tile.transform.position = new Vector2(tile.transform.position.x, tile.transform.position.y - 1f);
-            }
-            
-        }
-        //array3 = new int[] { 1, 3, 5, 7, 9 };
-        ++numTiles;
-        lavaFlow = false;
-    }
-
-    struct colPara
-    {
-        public int numTiles;
-        //public int[] skip;
         public int skip;
         public int xPos;
+        public List<GameObject> activeLavaPrefabs;
+        public bool isPattern;
+        public int[] pattern;
+
+        public StructData(int skip, int xPos, List<GameObject> activeLavaPrefabs, bool isPattern, int[] pattern)
+        {
+            this.skip = skip;
+            this.xPos = xPos;
+            this.activeLavaPrefabs = activeLavaPrefabs;
+            this.isPattern = isPattern;
+            this.pattern = pattern;
+        }
     }
 
-    private void lavaCol25(colPara data)
+    private void prepareLava()
     {
-        //yield return new WaitForSeconds(lavaFallDelay);
-        Debug.Log("Making new tile!");
-        //Debug.Log("numtiles:" + numTiles);
-        //firstlist.Insert(numTiles, Instantiate(lavaPreFab, new Vector3(122.27f, -14.71f, 14), Quaternion.identity) as GameObject);
-        //if(numTiles % 3 == 0)
-        //{
-        //    firstlist.Add(Instantiate(lavaPreFab, new Vector3(122.27f, -14.71f, 14), Quaternion.identity) as GameObject);
-        //}
-        Debug.Log("Where xpos:" + data.xPos);
-        Debug.Log("Where colXMultiplier:" + colXMultiplier);
-        Debug.Log("Where colXMultiplier * data.xPos" + colXMultiplier * data.xPos);
-        Debug.Log("Where colXStart:" + colXStart);
-        Debug.Log("Where (colXMultiplier * data.xPos) + colXStart:" + ((colXMultiplier * data.xPos) + colXStart));
-        Debug.Log("Moving instance to this X: " + ((colXMultiplier * data.xPos) + colXStart));
-        //if (data.numTiles % data.skip == 0)
-        //{
-        //    firstlist.Add(Instantiate(lavaPreFab, new Vector3(colXStart+(colXMultiplier*data.xPos), -14.71f, 14), Quaternion.identity) as GameObject);
-        //}
-        firstlist.Add(Instantiate(lavaPreFab, new Vector3(colXStart + (colXMultiplier * data.xPos), -15.644f, 14), Quaternion.identity) as GameObject);
+        int[] whichColPattern = new int[] { 12, 13, 14, 15, 16, 17 ,18 };
+        int[] patternCol12 = new int[1000];
+        int[] patternCol13 = new int[1000];
+        int[] patternCol14 = new int[1000];
+        int[] patternCol15 = new int[1000];
+        int[] patternCol16 = new int[1000];
+        int[] patternCol17 = new int[1000];
+        int[] patternCol18 = new int[1000];
+        patternCol12[0] = 1;
+        patternCol12[1] = 2;
+        patternCol12[2] = 3;
+        patternCol13[0] = 1;
+        patternCol13[1] = 2;
+        patternCol13[2] = 3;
+        patternCol13[3] = 4;
+        patternCol13[4] = 5;
+        patternCol13[5] = 6;
+        patternCol13[6] = 16;
+        patternCol13[7] = 17;
+        patternCol13[8] = 18;
+        patternCol14[0] = 1;
+        patternCol14[1] = 2;
+        patternCol14[2] = 3;
+        patternCol14[3] = 4;
+        patternCol14[4] = 5;
+        patternCol14[5] = 6;
+        patternCol14[6] = 7;
+        patternCol14[7] = 8;
+        patternCol14[8] = 9;
+        patternCol14[9] = 13;
+        patternCol14[10] = 14;
+        patternCol14[11] = 15;
+        patternCol14[12] = 16;
+        patternCol14[13] = 17;
+        patternCol14[14] = 18;
+        patternCol15[0] = 1;
+        patternCol15[1] = 2;
+        patternCol15[2] = 3;
+        patternCol15[3] = 4;
+        patternCol15[4] = 5;
+        patternCol15[5] = 6;
+        patternCol15[6] = 7;
+        patternCol15[7] = 8;
+        patternCol15[8] = 9;
+        patternCol15[9] = 10;
+        patternCol15[10] = 11;
+        patternCol15[11] = 12;
+        patternCol15[12] = 13;
+        patternCol15[13] = 14;
+        patternCol15[14] = 15;
+        patternCol15[15] = 16;
+        patternCol15[16] = 17;
+        patternCol15[17] = 18;
+        patternCol16[0] = 4;
+        patternCol16[1] = 5;
+        patternCol16[2] = 6;
+        patternCol16[3] = 7;
+        patternCol16[4] = 8;
+        patternCol16[5] = 9;
+        patternCol16[6] = 10;
+        patternCol16[7] = 11;
+        patternCol16[8] = 12;
+        patternCol16[9] = 13;
+        patternCol16[10] = 14;
+        patternCol16[11] = 15;
+        patternCol16[12] = 16;
+        patternCol16[13] = 17;
+        patternCol16[14] = 18;
+        patternCol17[0] = 7;
+        patternCol17[1] = 8;
+        patternCol17[2] = 9;
+        patternCol17[3] = 10;
+        patternCol17[4] = 11;
+        patternCol17[5] = 12;
+        patternCol17[6] = 13;
+        patternCol17[7] = 14;
+        patternCol17[8] = 15;
+        patternCol18[0] = 10;
+        patternCol18[1] = 11;
+        patternCol18[2] = 12;
 
-        Debug.Log("Lava tile made");
-        //numTiles += 1;
-        for(int i = 0; i < firstlist.Count; ++i)// (GameObject tile in firstlist)
+
+        //col12
+        for (int i = 3; i < 1000; ++i)
         {
-            Debug.Log("List size: " + firstlist.Count);
-            Debug.Log("i:" + i);
-            GameObject tile = firstlist[i];
-            //Debug.Log(tile.transform.position.y);
-            Debug.Log("Tile Position: " + tile.transform.position);
+            patternCol12[i] = patternCol12[i - 3] + 18;//patternCol14[i - 3] + 6;
+        }
+        //col13
+        for (int i = 9; i < 1000; ++i)
+        { 
+            patternCol13[i] = patternCol13[i - 9] + 18;//patternCol14[i - 3] + 6;
+        }
+        //col14
+        for (int i = 15; i < 1000; ++i)
+        {
+            patternCol14[i] = patternCol14[i - 15] + 18;//patternCol14[i - 3] + 6;
+        }
+        //col15
+        for (int i = 18; i < 1000; ++i)
+        {
+            patternCol15[i] = patternCol15[i - 18] + 18;//patternCol14[i - 3] + 6;
 
-            if (tile.transform.position.y + 30.71f < 0.01)
-            { 
-                Destroy(tile); 
-                firstlist.Remove(tile); 
-                Debug.Log("Destroy!"); 
+        }
+        //col16
+        for (int i = 15; i < 1000; ++i)
+        {
+            patternCol16[i] = patternCol16[i - 15] + 18;//patternCol14[i - 3] + 6;
+        }
+        //col17
+        for (int i = 9; i < 1000; ++i)
+        {
+            patternCol17[i] = patternCol17[i - 9] + 18;//patternCol14[i - 3] + 6;
+        }
+        //col18
+        for (int i = 3; i < 1000; ++i)
+        {
+            patternCol18[i] = patternCol18[i - 3] + 18;//patternCol14[i - 3] + 6;
+        }
+
+        //for (int i = 0; i < 100; ++i)
+        //{
+        //    Debug.Log(patternCol16[i]);
+        //}
+        for (int columnStruct = 0; columnStruct < struct31List.Length; ++columnStruct)
+        {
+            StructData currentStruct = new StructData(1, columnStruct, new List<GameObject>(), false, new int[] {-1});
+
+            if (whichColPattern.Contains(columnStruct) && columnStruct == 12)
+            {
+                currentStruct = new StructData(1, columnStruct, new List<GameObject>(), true, patternCol12);
+            }
+            else if (whichColPattern.Contains(columnStruct) && columnStruct == 13)
+            {
+                currentStruct = new StructData(1, columnStruct, new List<GameObject>(), true, patternCol13);
+            }
+            else if(whichColPattern.Contains(columnStruct) && columnStruct == 14)
+            {
+                currentStruct = new StructData(1, columnStruct, new List<GameObject>(), true, patternCol14);
+            }
+            else if(whichColPattern.Contains(columnStruct) && columnStruct == 15)
+            {
+                currentStruct = new StructData(1, columnStruct, new List<GameObject>(), true, patternCol15);
+
+            }
+            else if(whichColPattern.Contains(columnStruct) && columnStruct == 16)
+            {
+                currentStruct = new StructData(1, columnStruct, new List<GameObject>(), true, patternCol16);
+            }
+            else if (whichColPattern.Contains(columnStruct) && columnStruct == 17)
+            {
+                currentStruct = new StructData(1, columnStruct, new List<GameObject>(), true, patternCol17);
+            }
+            else if (whichColPattern.Contains(columnStruct) && columnStruct == 18)
+            {
+                currentStruct = new StructData(1, columnStruct, new List<GameObject>(), true, patternCol18);
+            }
+
+            struct31List[columnStruct] = currentStruct;
+        }
+    }
+    IEnumerator releaseLava()
+    {
+        if(tileNum == 48)
+        {
+            tilesToDelete = GameObject.FindGameObjectsWithTag("Wave");
+
+            foreach (GameObject tile in tilesToDelete)
+            {
+                Destroy(tile);
+            }
+            StopCoroutine("releaseLava");
+        }
+
+        yield return new WaitForSeconds(.5f);
+        //yield return new WaitForSeconds(lavaFallDelay);
+        for (int col = 0; col < struct31List.Length; ++col)
+        {
+            bool foundBlankTile = false;
+
+            for (int i = 0; i < struct31List[col].pattern.Length; ++i)
+            {
+                //Debug.Log("tileNum: " + tileNum);
+                //Debug.Log("Column: " + col);
+                //Debug.Log("Value: " + struct31List[col].pattern[i]);
+                foundBlankTile = struct31List[col].pattern[i] == tileNum;
+                //Debug.Log("foundBlankTile: " + foundBlankTile);
+                if (foundBlankTile) { break ;}
+            }
+            //Debug.Log("foundBlankAgainTile: " + foundBlankTile);
+
+            GameObject tile;
+
+            if (!foundBlankTile)//!struct31List[col].isPattern
+            {
+                //Debug.Log("Tile made!");
+                tile = Instantiate(lavaPreFab, new Vector3(colXStart + (colXMultiplier * struct31List[col].xPos), -14.644f, 14), Quaternion.identity) as GameObject;
+                struct31List[col].activeLavaPrefabs.Add(tile);
             }
             else
             {
-                tile.transform.position = new Vector2(tile.transform.position.x, tile.transform.position.y - 1f);
+                //Debug.Log("Should be blank tile");
             }
-            Debug.Log("Tile Position after -1: " + tile.transform.position);
-            //Debug.Log("Made it");
+
+            for (int activeTile = 0; activeTile < struct31List[col].activeLavaPrefabs.Count; ++activeTile)
+            {
+                tile = struct31List[col].activeLavaPrefabs[activeTile];
+                if (tile.transform.position.y + 29.71f < 0.01f)
+                {
+                    Destroy(tile);
+                    struct31List[col].activeLavaPrefabs.Remove(tile);
+                    //Debug.Log("Destroy!");
+                }
+                else
+                {
+                    tile.transform.position = new Vector2(tile.transform.position.x, tile.transform.position.y - 1f);
+                }
+            }
         }
+        ++tileNum;
+
         lavaFlow = false;
     }
 }
